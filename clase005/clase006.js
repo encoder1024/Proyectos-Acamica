125d0410c096c9d93f5d637ef35fd5067df026d4
unknown <andresferrer256@gmail.com>
2019-09-02 21:03:44 -0300

    clase006-01

---
+++ clase005/clase006.js
@@ -0,0 +1,6 @@
+console.log("Mi primera aplicación web!!! desde un archivo js!");
+var nombre = "Andrés";
+var anio = 2019;
+var cursando = true;
+console.log(nombre, anio, cursando);
+alert(nombre);
